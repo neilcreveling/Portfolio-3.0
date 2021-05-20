7f383c133df89f32232fe22c1ae1c51d3dc0b6f5
Neil Creveling <neil.creveling@gmail.com>
2021-05-20 16:53:26 -0400

    main

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+app.set('views', __dirname + '/views');
+
+app.set('view engine', 'handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about')
+});
+
+app.get('/work', (req, res) => {
+    res.render('work')
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact')
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(8080, () => {
+    console.log('The web server has started on port 8080');
+});
